20ada457b2e03c4848a1867a1bfd9853ea972d71
leenorshn <katembovictor@gmail.com>
2020-09-12 17:21:09 +0100

    adding emit event after votes created

--- src/controllers/vote_controller.ts
+++ src/controllers/vote_controller.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
+const {getIo} = require ("./../ioConfig")
 
 const db = new PrismaClient()
 
@@ -21,7 +22,7 @@ const db = new PrismaClient()
                 created_at:Date.now()
                 
             }})
-            //webSocket hire
+            getIo().emit("votes",{vote});
             return res.status(201).json(vote);
     } catch (error) {
         return res.status(500).json(error);
